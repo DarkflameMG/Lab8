ecd4a7cd23a56cf338693d01e4cdfa3f4660cc95
DarkflameMG <deknan70@gmail.com>
2021-01-05 15:13:27 +0700

    lab8_1 completed

--- lab8_1.cpp
+++ lab8_1.cpp
@@ -3,9 +3,14 @@
 using namespace std;
 
 int main(){	
+	int year=1;
+	double loan,rate,can_pay,interest,total;
 	cout << "Enter initial loan: ";
+	cin >> loan;
 	cout << "Enter interest rate per year (%): ";
+	cin >> rate;
 	cout << "Enter amount you can pay per year: ";
+	cin >> can_pay;
 
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
@@ -20,14 +25,28 @@ int main(){
 	
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
+	do{
+		interest = loan*rate/100;
+		total = loan+interest;
+		cout << fixed << setprecision(2); 
+		cout << setw(13) << left << year; 
+		cout << setw(13) << left << loan;
+		cout << setw(13) << left << interest;
+		cout << setw(13) << left << total;
+		if(total > can_pay)
+		{
+			cout << setw(13) << left << can_pay;
+			loan = total-can_pay;
+		}
+		else
+		{
+			cout << setw(13) << left << total;
+			loan = 0;
+		}
+		cout << setw(13) << left << loan;
+		cout << "\n";	
+		year++;
+	}while(loan > 0);
 	
 	return 0;
 }
